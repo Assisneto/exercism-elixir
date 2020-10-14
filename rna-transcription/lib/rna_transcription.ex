b045e80a9f8de351f894f7414c467521992984a0
Assisneto Damasceno <netodamasceno12@gmail.com>
2020-10-14 08:45:23 -0300

    refact rna-transcription to using charlist

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -9,17 +9,16 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    List.to_string(dna)
-      |> String.graphemes()
+
+      dna
       |> Enum.map(fn (x)->
         case x do
-          "G" -> 'C'
-          "C" -> 'G'
-          "T" -> 'A'
-          "A" -> 'U'
+          ?G -> ?C
+          ?C -> ?G
+          ?T -> ?A
+          ?A -> ?U
           end
       end)
-      |> Enum.join()
-      |> String.to_charlist()
+
     end
 end
